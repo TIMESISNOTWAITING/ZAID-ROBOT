17161a649ed6f1d7c690a1a6fc404f7b8e29dc82
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:24:29 +0530

    Update nsfw_sql.py

--- Zaid/modules/sql/nsfw_sql.py
+++ Zaid/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from EmikoRobot.modules.sql import BASE, SESSION
+from Zaid.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
