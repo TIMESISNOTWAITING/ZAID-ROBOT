5b1aa83273a3eb3c649365f8822bff4ba40a83ac
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-11 22:19:56 +0530

    Update styletext.py

--- Zaid/modules/styletext.py
+++ Zaid/modules/styletext.py
@@ -1,6 +1,6 @@
-from EmikoRobot  import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.alternate import typing_action
+from Zaid  import dispatcher
+from Zaid.modules.disable import DisableAbleCommandHandler
+from Zaid.modules.helper_funcs.alternate import typing_action
 from telegram import ParseMode
 from telegram.ext import run_async
 
