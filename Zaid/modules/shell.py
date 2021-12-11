4d79a62d517f06166158c08f988bde5eb8a4775f
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-12 00:39:55 +0530

    Update shell.py

--- Zaid/modules/shell.py
+++ Zaid/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from EmikoRobot import LOGGER, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+from Zaid import LOGGER, dispatcher
+from Zaid.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
