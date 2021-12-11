52ee2087ba176cc33abf6069dd42dc608b7fe59e
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:09:09 +0530

    Update chatstatus.py

--- Zaid/modules/helper_funcs/telethn/chatstatus.py
+++ Zaid/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from EmikoRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from EmikoRobot import DRAGONS
+from Zaid.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from Zaid import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
