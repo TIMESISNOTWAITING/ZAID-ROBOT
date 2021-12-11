de364b8391cffb2dd89fa24939fd02e2e91933ee
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:20:49 +0530

    Update locks_sql.py

--- Zaid/modules/sql/locks_sql.py
+++ Zaid/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from EmikoRobot.modules.sql import SESSION, BASE
+from Zaid.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
