388da9fab9017b795e237504c92d2bd7cc545e95
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-12 01:02:49 +0530

    Update heroku.py

--- Zaid/modules/heroku.py
+++ Zaid/modules/heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from EmikoRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from EmikoRobot.events import register
+from Zaid import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from Zaid.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
