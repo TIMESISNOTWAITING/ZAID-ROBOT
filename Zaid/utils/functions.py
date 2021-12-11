553d99666bf7257410af537dab67861ea042918c
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:05:17 +0530

    Update functions.py

--- Zaid/utils/functions.py
+++ Zaid/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from EmikoRobot import aiohttpsession as aiosession
-from EmikoRobot.ex_plugins.dbfunctions import start_restart_stage
-from EmikoRobot.utils.http import get, post
+from Zaid import aiohttpsession as aiosession
+from Zaid.ex_plugins.dbfunctions import start_restart_stage
+from Zaid.utils.http import get, post
 
 
 async def restart(m: Message):
