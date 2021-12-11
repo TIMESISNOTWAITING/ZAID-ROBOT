2a0ca42fc7d057bf8c730984ecf81f56d6eafb0d
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:19:38 +0530

    Update feds_sql.py

--- Zaid/modules/sql/feds_sql.py
+++ Zaid/modules/sql/feds_sql.py
@@ -1,8 +1,8 @@
 import ast
 import threading
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.sql import BASE, SESSION
+from Zaid import dispatcher
+from Zaid.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 from sqlalchemy.sql.sqltypes import BigInteger
