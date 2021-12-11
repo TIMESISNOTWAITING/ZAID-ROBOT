2c11de7f150a5c2967294dbae9225562632e24aa
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-12 01:08:51 +0530

    Update karma.py

--- Zaid/modules/karma.py
+++ Zaid/modules/karma.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
 
-from EmikoRobot import pbot as app, BOT_ID
-from EmikoRobot.utils.errors import capture_err
-from EmikoRobot.utils.permissions import adminsOnly
-from EmikoRobot.ex_plugins.dbfunctions import (
+from Zaid import pbot as app, BOT_ID
+from Zaid.utils.errors import capture_err
+from Zaid.utils.permissions import adminsOnly
+from Zaid.ex_plugins.dbfunctions import (
     alpha_to_int,
     get_karma,
     get_karmas,
@@ -16,8 +16,8 @@ from EmikoRobot.ex_plugins.dbfunctions import (
     karma_on,
     update_karma,
 )      
-from EmikoRobot.utils.filter_groups import karma_negative_group, karma_positive_group
-from EmikoRobot import arq
+from Zaid.utils.filter_groups import karma_negative_group, karma_positive_group
+from Zaid import arq
 
 regex_upvote = r"^((?i)\+|\+\+|\+1|thx|thanx|thanks|pro|cool|good|👍)$"
 regex_downvote = r"^(\-|\-\-|\-1|👎|noob|weak)$"
