ec36ff6852e80bce648bac6e2f194ea214a8e956
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:37:28 +0530

    Update blacklist.py

--- Zaid/modules/blacklist.py
+++ Zaid/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import EmikoRobot.modules.sql.blacklist_sql as sql
-from EmikoRobot import dispatcher, LOGGER
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from EmikoRobot.modules.helper_funcs.extraction import extract_text
-from EmikoRobot.modules.helper_funcs.misc import split_message
-from EmikoRobot.modules.log_channel import loggable
-from EmikoRobot.modules.warns import warn
-from EmikoRobot.modules.helper_funcs.string_handling import extract_time
-from EmikoRobot.modules.connection import connected
-from EmikoRobot.modules.sql.approve_sql import is_approved
-from EmikoRobot.modules.helper_funcs.alternate import send_message, typing_action
+import Zaid.modules.sql.blacklist_sql as sql
+from Zaid import dispatcher, LOGGER
+from Zaid.modules.disable import DisableAbleCommandHandler
+from Zaid.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Zaid.modules.helper_funcs.extraction import extract_text
+from Zaid.modules.helper_funcs.misc import split_message
+from Zaid.modules.log_channel import loggable
+from Zaid.modules.warns import warn
+from Zaid.modules.helper_funcs.string_handling import extract_time
+from Zaid.modules.connection import connected
+from Zaid.modules.sql.approve_sql import is_approved
+from Zaid.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
