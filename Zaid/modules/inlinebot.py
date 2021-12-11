a812562ecf06268964efdef3665d496dec27c902
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-12 01:06:32 +0530

    Update inlinebot.py

--- Zaid/modules/inlinebot.py
+++ Zaid/modules/inlinebot.py
@@ -1,8 +1,8 @@
 import traceback
 
-from EmikoRobot import pbot as app
-from EmikoRobot.utils.pluginhelper import fetch
-from EmikoRobot.utils.inlinefuncs import *
+from Zaid import pbot as app
+from Zaid.utils.pluginhelper import fetch
+from Zaid.utils.inlinefuncs import *
 
 __MODULE__ = "Inline"
 __HELP__ = """See inline for help related to inline"""
