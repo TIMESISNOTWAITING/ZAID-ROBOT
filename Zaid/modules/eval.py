bedc23d9e07914bcf52ad5ef37fb7b52731808f4
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 23:17:11 +0530

    Update eval.py

--- Zaid/modules/eval.py
+++ Zaid/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from EmikoRobot import LOGGER, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+from Zaid import LOGGER, dispatcher
+from Zaid.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
