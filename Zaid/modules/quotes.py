c5a18b22a06604b70bbb6f4bbecd41be9614cf5a
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-12 00:57:51 +0530

    Update quotes.py

--- Zaid/modules/quotes.py
+++ Zaid/modules/quotes.py
@@ -4,9 +4,9 @@ from traceback import format_exc
 from pyrogram import filters
 from pyrogram.types import Message
 
-from EmikoRobot import arq
-from EmikoRobot.utils.errors import capture_err
-from EmikoRobot import pbot as app
+from Zaid import arq
+from Zaid.utils.errors import capture_err
+from Zaid import pbot as app
 
 
 async def quotify(messages: list):
