bbd23cccda30b1fb7329f108dbc90b54ec4dda84
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 21:34:58 +0530

    Update config.py

--- Zaid/config.py
+++ Zaid/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/EmikoRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Zaid/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "emikosupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1669178360  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Timesisnotwaiting"
+    SUPPORT_CHAT = "Superior_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001432609692
     )  # Prints any new group the bot is added to, prints just the name and ID.
