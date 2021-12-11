492e95610d569d123e4ad27fa3f4441f1f874e88
AMANTYA1 <95975318+AMANTYA1@users.noreply.github.com>
2021-12-12 01:05:10 +0530

    Update info.py

--- Zaid/modules/info.py
+++ Zaid/modules/info.py
@@ -3,9 +3,9 @@ import os
 from pyrogram import filters
 from pyrogram.types import Message
 
-from EmikoRobot import DEV_USERS
-from EmikoRobot import pbot as app
-from EmikoRobot.services.sections import section
+from Zaid import DEV_USERS
+from Zaid import pbot as app
+from Zaid.services.sections import section
 
 
 async def get_user_info(user, already=False):
