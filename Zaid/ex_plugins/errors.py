39d8ee184c41950df719d9ca1c7ac4f037b28f33
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 21:38:40 +0530

    Update errors.py

--- Zaid/ex_plugins/errors.py
+++ Zaid/ex_plugins/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from EmikoRobot import EVENT_LOGS, pbot as app
+from Zaid import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
