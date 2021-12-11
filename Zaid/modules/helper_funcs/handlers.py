706028ce25cb5d8b547f84ebf28735593b8bad50
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:13:36 +0530

    Update handlers.py

--- Zaid/modules/helper_funcs/handlers.py
+++ Zaid/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import EmikoRobot.modules.sql.blacklistusers_sql as sql
-from EmikoRobot import ALLOW_EXCL
-from EmikoRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import Zaid.modules.sql.blacklistusers_sql as sql
+from Zaid import ALLOW_EXCL
+from Zaid import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
