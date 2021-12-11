039528fdce07c6cd8f3c31768cd10c21973dcad5
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 21:37:03 +0530

    Update mongo.py

--- Zaid/mongo.py
+++ Zaid/mongo.py
@@ -3,8 +3,8 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from EmikoRobot import MONGO_DB_URI
-from EmikoRobot.confing import get_int_key, get_str_key
+from Zaid import MONGO_DB_URI
+from Zaid.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
