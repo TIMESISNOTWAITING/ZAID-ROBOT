5bc0838d053a1e4a069ef6e55027881d8ed71b20
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:16:02 +0530

    Update afk_sql.py

--- Zaid/modules/sql/afk_sql.py
+++ Zaid/modules/sql/afk_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from Zaid.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
