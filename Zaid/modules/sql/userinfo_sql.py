07436859e10fd0832dfe679ae0c22c2d85698e6a
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:23:15 +0530

    Update userinfo_sql.py

--- Zaid/modules/sql/userinfo_sql.py
+++ Zaid/modules/sql/userinfo_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from Zaid.modules.sql import BASE, SESSION
 from sqlalchemy import Column, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
