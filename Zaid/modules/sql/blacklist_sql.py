2f7c9835eefcaebda3c1991f7bd64a6e8d106df1
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 22:17:01 +0530

    Update blacklist_sql.py

--- Zaid/modules/sql/blacklist_sql.py
+++ Zaid/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from EmikoRobot.modules.sql import SESSION, BASE
+from Zaid.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
