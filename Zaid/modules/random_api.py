25be6390708b60affd34cba410184a6a2e336a4b
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-12 09:12:06 +0530

    Update random_api.py

--- Zaid/modules/random_api.py
+++ Zaid/modules/random_api.py
@@ -46,7 +46,7 @@ import urllib
 import asyncio
 import os
 from pyrogram import filters
-from EmikoRobot import TEMP_DOWNLOAD_DIRECTORY, pbot
+from Zaid import TEMP_DOWNLOAD_DIRECTORY, pbot
 
 
 @pbot.on_message(filters.command("boobs"))
