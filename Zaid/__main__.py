12f091ffe44627f4dd8295f9d3d631b1a575567e
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-12 12:44:53 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -82,12 +82,14 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 *Hello {} !*
-✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
+😈 HI'm Zaid, An anime - Themed advanced telegram Group management with lots of features.
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
 ────────────────────────
-✪ Hit /help to see my available commands.
+✪ Bot For help You Manage & Protect Your Groups.
+[➼](https://telegra.ph/file/be24bbabbe0ec30dff489.jpg) So What U Waiting For Add Me To Ur chat
+───────────────────────
 """
 
 buttons = [
