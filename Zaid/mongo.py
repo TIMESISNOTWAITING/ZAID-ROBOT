9b61551a0ebcbc2218825ddb70d17a98c550c298
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-13 11:23:13 +0530

    Update mongo.py

--- Zaid/mongo.py
+++ Zaid/mongo.py
@@ -6,8 +6,8 @@ from pymongo.errors import ServerSelectionTimeoutError
 from Zaid import MONGO_DB_URI
 
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_PORT = "27017"
+MONGO_DB_URI = "MONGO_DB_URI"
 MONGO_DB = "ZaidRobot"
 
 
