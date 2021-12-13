3296fe4fe56f917edc9988f4236b761b371b3aec
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-13 11:18:04 +0530

    Update gshield.py

--- Zaid/modules/gshield.py
+++ Zaid/modules/gshield.py
@@ -9,7 +9,6 @@ from better_profanity import profanity
 from google_trans_new import google_translator
 from telethon import events
 from telethon.tl.types import ChatBannedRights
-from Zaid.confing import get_int_key, get_str_key
 from Zaid.services.telethonbasics import is_admin
 from Zaid.events import register
 from pymongo import MongoClient
