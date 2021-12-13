8d2ba2086d3003b6ec2fc07b6a26074b90099116
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-13 11:27:49 +0530

    Update mongo.py

--- Zaid/mongo.py
+++ Zaid/mongo.py
@@ -12,10 +12,10 @@ MONGO_DB = "ZaidRobot"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+client = MongoClient(mongodb+srv://music:music@cluster0.sh6h4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority, 27017)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["Tgn_ro_bot"]
+db = client["ZaidRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
